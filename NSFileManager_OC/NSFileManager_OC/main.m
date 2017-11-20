494e836cd6994fcceb5499fcf436e0662d8e4afd
Felix <felix@angellecho.com>
2017-11-20 13:00:52 +0800

    no message

---
+++ NSFileManager_OC/NSFileManager_OC/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  NSFileManager_OC
+//
+//  Created by Felix on 2017/11/18.
+//  Copyright © 2017年 Felix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        //创建文件管理对象
+        NSFileManager* fm = [NSFileManager defaultManager];
+        //判断文件是否存在
+        
+    }
+    return 0;
+}
